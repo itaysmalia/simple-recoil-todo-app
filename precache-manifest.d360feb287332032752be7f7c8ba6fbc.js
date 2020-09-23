92e6582bca5e9aa5e2a7488b86adebabdffc8be4
Itay Sin-Malia <itay.sinmalia@brightmachines.com>
2020-09-23 23:45:36 +0300

    Updates

---
+++ precache-manifest.d360feb287332032752be7f7c8ba6fbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2857bb6c349c05f39c0f6ef48d74a420",
+    "url": "/simple-recoil-todo-app/index.html"
+  },
+  {
+    "revision": "6581b724f80867f68c7f",
+    "url": "/simple-recoil-todo-app/static/css/main.5c2bf307.chunk.css"
+  },
+  {
+    "revision": "80be6402ec50919bbdbd",
+    "url": "/simple-recoil-todo-app/static/js/2.c51a773e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/simple-recoil-todo-app/static/js/2.c51a773e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6581b724f80867f68c7f",
+    "url": "/simple-recoil-todo-app/static/js/main.de2ddab3.chunk.js"
+  },
+  {
+    "revision": "4ab434b0dda7f8789b5e",
+    "url": "/simple-recoil-todo-app/static/js/runtime-main.6358d7d2.js"
+  }
+]);
